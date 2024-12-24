b823e99d13b2b669cd8252eb028f76c6e7cd319e
Livio Conzett <5398351+LivioConzett@users.noreply.github.com>
2024-12-24 14:08:20 +0100

    finisched part 1 of day 3

--- 2024/day_3/part_1/main.c
+++ 2024/day_3/part_1/main.c
@@ -20,6 +20,59 @@ enum State{
 // current state
 enum State current_state = START;
 
+// create the two strings to save the number to
+char first_num[100];
+int first_num_counter = 0;
+
+char second_num[100];
+int second_num_counter = 0;
+
+int total_sum = 0;
+
+/**
+ * \brief check if a character is a number from 0 - 9
+ * \param c character to check
+ * \return 0 if character is not a number, 1 if it is
+ */
+int is_a_number(char c){
+
+    int num = (int) c;
+
+    if(num >= 48 && num <= 57) return 1;
+
+    return 0;
+}
+
+/**
+ * \brief add a character to the end of the first number string
+ * \param c character to append
+ */
+void append_to_first_number(char c){
+    
+    first_num[first_num_counter] = c;
+    first_num_counter++;
+}
+
+/**
+ * \brief reset the first and second number
+ */
+void reset_numbers(){
+    // reset the first and second number
+    first_num[0] = 0;
+    first_num_counter = 0;
+    second_num[0] = 0;
+    second_num_counter = 0;
+}
+
+/**
+ * \brief add a character to the end of the second number string
+ * \param c character to append
+ */
+void append_to_second_number(char c){
+    
+    second_num[second_num_counter] = c;
+    second_num_counter++;
+}
 
 
 /**
@@ -27,6 +80,9 @@ enum State current_state = START;
  * \param c the character that was read
  */
 void start_function(char c){
+
+    reset_numbers();
+
     if(c == 'm') current_state = M_STATE;
 }
 
@@ -39,7 +95,155 @@ void m_function(char c){
     else current_state = START;
 }
 
+/**
+ * \brief Handel the u state
+ * \param c character that was read
+ */
+void u_function(char c){
+    if(c == 'l') current_state = L_STATE;
+    else current_state = START;
+}
 
+/**
+ * \brief Handel the l state
+ * \param c character that was read
+ */
+void l_function(char c){
+    if(c == '(') current_state = LB_STATE;
+    else current_state = START;
+}
+
+/**
+ * \brief Handel the ( state
+ * \param c character that was read
+ */
+void lb_function(char c){
+
+    if(c == '-'){
+        current_state = FIRST_MINUS_STATE;
+        append_to_first_number(c);
+        return;
+    }
+
+    if(is_a_number(c)){
+        current_state = FIRST_NUM_STATE;
+        append_to_first_number(c);
+        return;
+    }
+
+    current_state = START;
+
+}
+
+/**
+ * \brief Handel the first minus state
+ * \param c charater that was read
+ */
+void first_minus_function(char c){
+    if(is_a_number(c)){
+        current_state = FIRST_NUM_STATE;
+        append_to_first_number(c);
+        return;
+    }
+
+    current_state = START;
+}
+
+/**
+ * \brief Handel the first number state
+ * \param c character that was read
+ */
+void first_number_function(char c){
+    
+    if(c == ','){
+        current_state = COMMA_STATE;
+        return;
+    }
+    
+    if(is_a_number(c)){
+        append_to_first_number(c);
+        return;
+    }
+
+    current_state = START;
+}
+
+/**
+ * \brief Handel the comma state
+ * \param c charater that was read
+ */
+void comma_function(char c){
+
+    if(c == '-'){
+        current_state = SECOND_MINUS_STATE;
+        append_to_second_number(c);
+        return;
+    }
+
+    if(is_a_number(c)){
+        current_state = SECOND_NUM_STATE;
+        append_to_second_number(c);
+        return;
+    }
+
+    current_state = START;
+}
+
+/**
+ * \brief Handel the second minus state
+ * \param c character that was read
+ */
+void second_minus_function(char c){
+    
+    if(is_a_number(c)){
+        current_state = SECOND_NUM_STATE;
+        append_to_second_number(c);
+        return;
+    }
+
+    current_state = START;
+}
+
+/**
+ * \brief Handel the second number state
+ * \param c character that was read
+ */
+void second_number_function(char c){
+    
+    if(c == ')'){
+        current_state = RB_STATE;
+        return;
+    }
+    
+
+    if(is_a_number(c)){
+        append_to_second_number(c);
+        return;
+    }
+
+    current_state = START;
+}
+
+/**
+ * \brief handel the ) state
+ * \param c character that was read
+ */
+void rb_function(char c){
+
+    // terminate the string
+    first_num[first_num_counter] = 0;
+    second_num[second_num_counter] = 0;
+
+    int first = atoi(first_num);
+    int second = atoi(second_num);
+
+    total_sum += first * second;
+
+    reset_numbers();
+
+    if(c == 'm') current_state = M_STATE;
+    else current_state = START;
+}
 
 
 /**
@@ -67,39 +271,51 @@ int main(int argc, char* argv[]){
     // go through the file char by char
     while((c = fgetc(file_ptr)) != EOF){
 
-        // create the two strings to save the number to
-        char first_num[100];
-        int first_num_counter = 0;
-
-        char second_num[100];
-        int second_num_counter = 0;
-
-
         switch(current_state){
 
             case START:
+                // printf("- start -\n");
                 start_function(c);
                 break;
             case M_STATE:
+                // printf("- M -\n");
                 m_function(c);
                 break;
             case U_STATE:
+                // printf("- U -\n");
+                u_function(c);
                 break;
             case L_STATE:
+                // printf("- L -\n");
+                l_function(c);
                 break;
             case LB_STATE:
+                // printf("- ( -\n");
+                lb_function(c);
                 break;
             case FIRST_MINUS_STATE:
+                // printf("- 1- -\n");
+                first_minus_function(c);
                 break;
             case FIRST_NUM_STATE:
+                // printf("- 1 num -\n");
+                first_number_function(c);
                 break;
             case COMMA_STATE:
+                // printf("- , -\n");
+                comma_function(c);
                 break;
             case SECOND_MINUS_STATE:
+                // printf("- 2- -\n");
+                second_minus_function(c);
                 break;
             case SECOND_NUM_STATE:
+                // printf("- 2 num -\n");
+                second_number_function(c);
                 break;
             case RB_STATE:
+                // printf("- ) -\n");
+                rb_function(c);
                 break;
             default:
 
@@ -109,6 +325,8 @@ int main(int argc, char* argv[]){
 
 
     fclose(file_ptr);
+
+    printf("total: %d\n", total_sum);
 
     return 0;
 }
